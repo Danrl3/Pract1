60e6e0471e2afa4ce626640d0782eed33c3e52c6
Danrl3 <146312932+Danrl3@users.noreply.github.com>
2023-10-05 12:42:56 -0600

    Update Prácticadan1.ino

--- Prácticadan1.ino
+++ Prácticadan1.ino
@@ -1,7 +1,9 @@
-void setup() { 
+void setup()
+{ 
 pinMode(LED_BUILTIN, OUTPUT); 
 }
-void loop() {
+void loop() 
+{
 digitalWrite(LED_BUILTIN, HIGH);
 delay(1000); 
 digitalWrite(LED_BUILTIN, LOW);
